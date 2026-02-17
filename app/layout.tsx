55a7a6301555e809d46b035b538611b8adc3f7d5
Rajat Singh <s.rajat55@gmail.com>
2026-02-17 15:11:41 +0530

    added reactbits bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
+import FloatingLines from "@/components/FloatingLines";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,16 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <div className="relative flex min-h-screen flex-col">
+            <div className="fixed inset-0 -z-10 h-full w-full">
+              <FloatingLines
+                linesGradient={["#2E0249", "#570A57", "#A91079"]}
+                lineCount={[4, 6, 4]}
+                lineDistance={[0.1, 0.2, 0.1]}
+                animationSpeed={0.5}
+                parallaxStrength={0.1}
+                mixBlendMode="normal"
+              />
+            </div>
             <Navbar />
             <main className="flex-1">
               {children}
